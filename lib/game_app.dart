2ce4925d441774023e54a7d9fed98ccbb55dba52
Kaushik Sharma <kaushik8695@gmail.com>
2023-11-22 20:44:40 +0530

    Add screen util package

--- lib/game_app.dart
+++ lib/game_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/constants/app_metadata.dart';
 import 'core/styles/app_themes.dart';
@@ -9,15 +10,18 @@ class GameApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: kAppName,
-      theme: AppThemes.dark,
-      navigatorKey: kNavigatorKey,
-      initialRoute: AppRoutes.splash,
-      onGenerateRoute: CustomNavigator.onGenerateRoute,
-      scrollBehavior: ScrollConfiguration.of(context).copyWith(
-        physics: const BouncingScrollPhysics(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 800),
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: kAppName,
+        theme: AppThemes.dark,
+        navigatorKey: kNavigatorKey,
+        initialRoute: AppRoutes.splash,
+        onGenerateRoute: CustomNavigator.onGenerateRoute,
+        scrollBehavior: ScrollConfiguration.of(context).copyWith(
+          physics: const BouncingScrollPhysics(),
+        ),
       ),
     );
   }
