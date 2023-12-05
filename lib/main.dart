c23f5468aded6a6f89e92da04b0ab27613f43e72
Kaushik Sharma <kaushik8695@gmail.com>
2023-12-05 14:19:27 +0530

    Fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'game_app.dart';
+import 'core/constants/app_metadata.dart';
+import 'core/styles/app_themes.dart';
+import 'core/widgets/custom_scroll_behavior.dart';
+import 'routes/custom_navigator.dart';
 
 void main() {
   runApp(const GameApp());
+}
+
+class GameApp extends StatelessWidget {
+  const GameApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 800),
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: kAppName,
+        theme: AppThemes.dark,
+        navigatorKey: kNavigatorKey,
+        initialRoute: AppRoutes.splash,
+        onGenerateRoute: CustomNavigator.onGenerateRoute,
+        scrollBehavior: const CustomScrollBehavior(),
+      ),
+    );
+  }
 }
